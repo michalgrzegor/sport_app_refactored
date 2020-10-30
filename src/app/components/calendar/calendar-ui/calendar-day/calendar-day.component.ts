90b59000be50d6ad3713fa88ba516aedfe482b6a
michalgrzegor <michalgrzegor@gmail.com>
2020-10-30 13:11:57 +0100

    added style to opened day calendar element

--- src/app/components/calendar/calendar-ui/calendar-day/calendar-day.component.ts
+++ src/app/components/calendar/calendar-ui/calendar-day/calendar-day.component.ts
@@ -1,10 +1,16 @@
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   Input,
   OnInit,
 } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { CalendarDay } from 'src/app/shared/models/calendar.interface';
+import { isEqual } from 'date-fns';
+import { getOpenedDay } from 'src/app/store/selectors/calendar-data.selectors';
 
 @Component({
   selector: 'app-calendar-day',
@@ -12,11 +18,19 @@ import { CalendarDay } from 'src/app/shared/models/calendar.interface';
   styleUrls: ['./calendar-day.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CalendarDayComponent implements OnInit {
+export class CalendarDayComponent implements OnInit, AfterViewInit {
   @Input() day: CalendarDay;
   @Input() isActualPage: boolean;
 
-  constructor() {}
+  public isOpened$: Observable<boolean>;
+
+  constructor(private store: Store) {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.isOpened$ = this.store
+      .select(getOpenedDay)
+      .pipe(map((cd) => isEqual(cd.date, this.day.date)));
+  }
 }
