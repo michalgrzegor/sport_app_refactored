5ad7a4ecb27303875c9aba3d98515baa5ac19dc9
michalgrzegor <michalgrzegor@gmail.com>
2020-11-17 13:49:56 +0100

    comment out buttons in left navbar | change auth config

--- src/app/shared/auth/config.ts
+++ src/app/shared/auth/config.ts
@@ -1,9 +1,16 @@
 export const CONFIG = {
   client_id: '47IJONQpYYxJa9SfynWN2Fgh7dJ4mvA-wDqHLF-RLSo',
-  // redirect_uri: 'http://localhost:4200/redirect',
   redirect_uri: 'https://goofy-elion-e84290.netlify.app/redirect',
   authorization_endpoint: 'oauth/authorize',
   token_endpoint: 'oauth/token',
   requested_scopes: 'openid',
   client_secret: 'GMaLKm0YXWuKKQiqNN9qhPv5np2yVlgI_9rCuaky2CI',
 };
+// export const CONFIG = {
+//   client_id: 'J_OqHJc9Vxqr1Ia6KyTA5YxiCXNJYpfs-yBWCbsWwrI',
+//   redirect_uri: 'http://localhost:4200/redirect',
+//   authorization_endpoint: 'oauth/authorize',
+//   token_endpoint: 'oauth/token',
+//   requested_scopes: 'openid',
+//   client_secret: 'R7caDJfmnXmrgP8a_J8FI1dYMQFuyvXLX1m1NTmSZdE',
+// };
